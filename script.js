2c42188a671d3289e449b51aac90e3c474c10aa5
ManuelDAssoro <73363119+ManuelDAssoro@users.noreply.github.com>
2025-03-29 17:58:43 -0300

    add create/clear/reset grid functionality, with custom size via user input

--- script.js
+++ script.js
@@ -1,20 +1,55 @@
-//Generate the 16x16 grid by inserting divs
-document.addEventListener("DOMContentLoaded", () => {
-    const container = document.getElementById("container");
-    const fragment = document.createDocumentFragment();
-
-    for (let i = 0; i < 255; i++){
-        const div = document.createElement("div")
-        fragment.appendChild(div);
-    }
-
-    container.appendChild(fragment);
+document.addEventListener("DOMContentLoaded", (event) => {
+    const createGridButton = document.getElementById("createGrid");
+    const clearGridButton = document.getElementById("clearGrid");
+    const resetGridButton = document.getElementById("resetGrid");
+    const gridContainer = document.getElementById("gridContainer");
+    createGridButton.addEventListener("click", gridGenerator);
+    clearGridButton.addEventListener("click", gridClear);
+    resetGridButton.addEventListener("click", gridReset);
+    gridReset();
 });
 
-/*
-const divs = document.querySelectorAll("div");
 
-divs.forEach((div) => {
-    div.addEventListener("hover", () => {    }); 
-});
-*/
+
+function gridClear(){
+    console.log("gridCleared");
+    const columns = gridContainer.querySelectorAll(".column");
+    columns.forEach(column => {
+        column.style.backgroundColor = '';
+    });
+}
+
+
+function gridGenerator(){
+    console.log("gridGenerated")
+    let gridSize = document.getElementById('gridInput').value
+    gridContainer.innerHTML = ""; 
+    for(let i = 0; i<gridSize; i++){
+        const row = document.createElement("div");
+        row.classList.add("row");
+        gridContainer.appendChild(row);
+        for(let j = 0; j<gridSize; j++){
+            const column = document.createElement("div");
+            column.classList.add("column");
+            row.appendChild(column);
+            column.addEventListener("mouseover", function() {
+                column.style.backgroundColor = "black";
+            });
+        }
+    }}
+
+function gridReset(){
+    gridContainer.innerHTML = ""; 
+    for(let i = 0; i<8; i++){
+        const row = document.createElement("div");
+        row.classList.add("row");
+        gridContainer.appendChild(row);
+        for(let j = 0; j<8; j++){
+            const column = document.createElement("div");
+            column.classList.add("column");
+            row.appendChild(column);
+            column.addEventListener("mouseover", function() {
+                column.style.backgroundColor = "black";
+            });
+        }
+}}
